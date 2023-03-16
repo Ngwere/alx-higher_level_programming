2c34909cb80ca8e635366642e43ceb8c6fa73eec
Ngwere <ngwerecliford@gmail.com>
2023-03-16 17:21:34 +0000

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-    if letter != (101 or 113):
+    if letter != 101 and letter != 113:
         print("{}".format(chr(letter)), end="")
