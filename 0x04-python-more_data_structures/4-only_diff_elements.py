7ad77e58b666840a3b4d6a5b83f61a589537e50c
Ngwere <ngwerecliford@gmail.com>
2023-03-18 17:41:32 +0000

    hot fixes

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
 
 def only_diff_elements(set_1, set_2):
-    """
-    Function that returns set of all ellements from one sest
-
-    @Args:
-        set_1 - set 1
-        set_2 - set 2
-        """
     return set_1 ^ set_2
