2f69d814838c61841d0d0845a0d45f6e00edba93
Ngwere <ngwerecliford@gmail.com>
2023-04-11 19:58:18 +0000

    added 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for inherits_from
+
+Contains the function that compaures object with class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a
+    class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    """
+
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
+
