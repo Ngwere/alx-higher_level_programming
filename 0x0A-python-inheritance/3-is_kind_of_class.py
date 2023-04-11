2e2aadfe11c3e69eee1c8a4727c832553b722cab
Ngwere <ngwerecliford@gmail.com>
2023-04-11 19:34:14 +0000

    error cleared

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,8 +4,10 @@
 Contains the function that compaures object with class.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
      Return true if object is instance of class or inherited from class
     """
+
     return isinstance(obj, a_class)
