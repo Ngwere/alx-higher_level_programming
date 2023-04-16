f8913f5c34de72a3f922c052427e0c690c99aa28
Ngwere <ngwerecliford@gmail.com>
2023-04-16 20:36:18 +0000

    corrected errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """the read_file module that contains the read_file() function"""
 
+
 def read_file(filename=""):
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
