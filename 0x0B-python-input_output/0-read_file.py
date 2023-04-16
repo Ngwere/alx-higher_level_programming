55f38f69a04ffa74848cb0ca81faca8a3e8e5f8f
Ngwere <ngwerecliford@gmail.com>
2023-04-16 20:52:48 +0000

    corrected some errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,11 @@
 
 
 def read_file(filename=""):
+    """
+    read file and print to standard output
+
+    Args:
+        filename (str) - the file name of the file to be used
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
