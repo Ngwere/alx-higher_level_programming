15322ef10c93b506aa6c9aade4ab09bbb5fb1723
Ngwere <ngwerecliford@gmail.com>
2023-06-20 00:37:08 +0000

    corrected model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,9 +12,12 @@ Base = declarative_base()
 
 
 class State(Base):
-    """Class representation of a State"""
+    """Class representation of a State
+
+     __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City',
-            backref='state', cascade='all, delete-orphan')
